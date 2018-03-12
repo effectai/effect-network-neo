585cffe408da2a3038e1a6962caede5c1952aae9
Jesse Eisses <jesse.eisses@gmail.com>
2018-03-12 07:48:06 +0100

    Add storage prefixes token lock

--- contracts/java/token-lock/src/ai/effect/token/TokenLock.java
+++ contracts/java/token-lock/src/ai/effect/token/TokenLock.java
@@ -20,9 +20,24 @@ public class TokenLock extends SmartContract
     public static final String RET_NO_OP = "no op";
     public static final String ARG_ERROR = "arguments invalid";
 
+    public static final String PREFIX_LOCK = "l";
+
     @Appcall(value=TOKEN_SCRIPT_HASH)
     public static Object token(String arg, Object[] args) {
         return null;
+    }
+
+    /**
+     * Generate a byte-array storage key. The prefix must be a 1-byte
+     * string used to prevent storage collisions; any additional byte
+     * arrays are concatenated.
+     */
+    public static byte[] storageKey(String prefix, byte[]... args) {
+        byte[] prefixArray = Helper.asByteArray(prefix);
+        byte[] key = Helper.concat(prefixArray, args[0]);
+        for (int i = 1; i < args.length; i++)
+            key = Helper.concat(key, args[i]);
+        return key;
     }
 
     /**
@@ -38,7 +53,8 @@ public class TokenLock extends SmartContract
     public static BigInteger getLockedBalance(byte[] address, BigInteger height) {
         if (address.length != 20) return null;
 
-        byte[] lockKey = Helper.concat(address, height.toByteArray());
+        byte[] lockKey = storageKey(PREFIX_LOCK, address, height.toByteArray());
+
         return new BigInteger(Storage.get(Storage.currentContext(), lockKey));
     }
 
@@ -60,9 +76,10 @@ public class TokenLock extends SmartContract
 
         byte[] lockAddress = getAddress();
         boolean transferred = (boolean) token("transfer", new Object[] {from, lockAddress, value});
+
         if (transferred == false) return false;
 
-        byte[] lockKey = Helper.concat(to, lockHeight.toByteArray());
+        byte[] lockKey = storageKey(PREFIX_LOCK, to, lockHeight.toByteArray());
         BigInteger lockValue = new BigInteger(Storage.get(Storage.currentContext(), lockKey));
 
         Storage.put(Storage.currentContext(), lockKey, lockValue.add(value));
@@ -74,17 +91,18 @@ public class TokenLock extends SmartContract
      * Unlock all tokens locked for `to` at `height`
      */
     public static boolean unlock(byte[] to, BigInteger height) {
-
         if (height.intValue() > Blockchain.height()) return false;
 
         if (to.length != 20) return false;
 
-        byte[] lockKey = Helper.concat(to, height.toByteArray());
+        byte[] lockKey = storageKey(PREFIX_LOCK, to, height.toByteArray());
         BigInteger value = new BigInteger(Storage.get(Storage.currentContext(), lockKey));
+
         if (value.equals(BigInteger.ZERO)) return false;
 
         byte[] lockAddress = getAddress();
         boolean transferred = (boolean) token("transfer", new Object[] {lockAddress, to, value});
+
         if (transferred == false) return false;
 
         Storage.delete(Storage.currentContext(), lockKey);
@@ -96,36 +114,42 @@ public class TokenLock extends SmartContract
      * Smart contract entrypoint
      */
     public static Object Main(String operation, Object[] args) {
-        if (operation == "address") return getAddress();
-        if (operation == "totalLocked") return getTotalLocked();
+        if (Runtime.trigger() == TriggerType.Verification) {
+            return true;
+        } else if (Runtime.trigger() == TriggerType.Application) {
+            if (operation == "address") return getAddress();
+            if (operation == "totalLocked") return getTotalLocked();
 
-        if (operation == "lockedBalanceAt") {
-            if (args.length != 2) return ARG_ERROR;
+            if (operation == "lockedBalanceAt") {
+                if (args.length != 2) return ARG_ERROR;
 
-            byte[] address = (byte[]) args[0];
-            BigInteger height = (BigInteger) args[1];
-            return getLockedBalance(address, height);
+                byte[] address = (byte[]) args[0];
+                BigInteger height = (BigInteger) args[1];
+                return getLockedBalance(address, height);
+            }
+
+            if (operation == "lock") {
+                if (args.length != 4) return ARG_ERROR;
+
+                byte[] from = (byte[]) args[0];
+                byte[] to = (byte[]) args[1];
+                BigInteger amount = (BigInteger) args[2];
+                BigInteger lockHeight = (BigInteger) args[3];
+
+                return lock(from, to, amount, lockHeight);
+            }
+
+            if (operation == "unlock") {
+                if (args.length != 2) return ARG_ERROR;
+
+                byte[] to = (byte[]) args[0];
+                BigInteger height = (BigInteger) args[1];
+                return unlock(to, height);
+            }
+
+            return RET_NO_OP;
         }
 
-        if (operation == "lock") {
-            if (args.length != 4) return ARG_ERROR;
-
-            byte[] from = (byte[]) args[0];
-            byte[] to = (byte[]) args[1];
-            BigInteger amount = (BigInteger) args[2];
-            BigInteger lockHeight = (BigInteger) args[3];
-
-            return lock(from, to, amount, lockHeight);
-        }
-
-        if (operation == "unlock") {
-            if (args.length != 2) return ARG_ERROR;
-
-            byte[] to = (byte[]) args[0];
-            BigInteger height = (BigInteger) args[1];
-            return unlock(to, height);
-        }
-
-        return RET_NO_OP;
+        return false;
     }
 }
